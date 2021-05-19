8104ba4f523e58fe4f0749a7a5d0302535767f82
guus-vrv <guusvrv@gmail.com>
2021-05-19 21:35:21 +0200

    Styled some more elements, added password changability, changed Index page.

--- App.js
+++ App.js
@@ -170,6 +170,8 @@ const AppContent = ( {navigation} ) => { // NavBar
 const Index = ( {navigation} ) => { // index page mananger, when user clicks login he arrives here
   const [visible, setVisible] = useState(false);// toggle overlay -> view card.
   const [amount, setAmount] = useState(0);
+  let today = new Date().toISOString().slice(0, 10);
+
   const toggleOverlay = () => { 
     setVisible(!visible);
   }  
@@ -209,16 +211,19 @@ const Index = ( {navigation} ) => { // index page mananger, when user clicks log
             <Card.Title>PURCHASE APPLES</Card.Title>
             <Card.Image source={require('./app/assets/poland.png')}/>
             <Card.Divider/>
-                  <Text style={{ marginBottom: 10}}>Click below to purchase fresh apples from Poland</Text>
+                  <Text style={{ marginBottom: 10, textAlign: 'center'}}>Click below to purchase fresh apples which were grown in Poland</Text>
                   <View style={styles.changeAmountBox}>
                     <TouchableOpacity style={styles.changeAmount, {marginLeft: 10}} onPress={onDecrement}>
-                      <Text style={{ fontSize: 20}}>-</Text>
+                      <Text style={{ fontSize: 20, color: '#000000'}}>-</Text>
                     </TouchableOpacity>
-                    <Text style={{textAlign: 'center', fontSize: 20}}>{amount}</Text>
+                    <Text style={{textAlign: 'center', fontSize: 20, color: '#6ECC77', fontWeight: 'bold'}}>{amount} pcs</Text>
                     <TouchableOpacity style={styles.changeAmount} onPress={onIncrement}>
-                      <Text style={{ fontSize: 20}}>+</Text>
+                      <Text style={{fontSize: 20, color: '#000000'}}>+</Text>
                     </TouchableOpacity>
                   </View>
+                  <Text style={{marginTop: 20}}>Price per apple: €1.094 </Text>
+                  <Text style={{marginTop: 20}}>Total cost of apples: €{Math.round((amount * 1.094) * 100) / 100}</Text>
+                  <Text style={{marginTop: 20, marginBottom: 20}}>Your cash: {10000}</Text>
                   <Button
                     color='#6ECC77'
                     icon={<Icon name='code' color='#ffffff' />}
@@ -234,7 +239,7 @@ const Index = ( {navigation} ) => { // index page mananger, when user clicks log
           <Card.Title>NEW APPLES</Card.Title>
           <Card.Image source={require('./app/assets/apple.jpg')}/>
           <Card.Divider/>
-                <Text style={{ marginBottom: 10}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam hendrerit pharetra sodales. In viverra risus vel elit efficitur mollis. Aliquam tempor justo in lorem mattis, in pharetra magna porttitor. Nulla mattis eget augue finibus aliquam. Quisque volutpat, ipsum et dictum volutpat, leo urna volutpat urna, vitae rutrum dolor metus vel ex.</Text>
+                <Text style={{ marginBottom: 10}}>Since 2012, part of the fruit manufactured and prepared for sale by group members has been sold through the Service2Fruit platform. The first batches of fruit sold in this way were industrial apples, delivered to the German and Dutch market. Currently, apples from Sadkowice (initially sorted or properly packaged) sold via the platform are delivered to final customers abroad as well as to local companies that act as middlemen in the fruit trade. One of the undeniable advantages of Servive2Fruit is the security of transactions. The service is licensed by a Central European bank so that no batch of goods can be sold without an advance payment.</Text>
                 <Button
                   color='#ff66ff'
                   icon={<Icon name='code' color='#ffffff' />}
@@ -243,16 +248,10 @@ const Index = ( {navigation} ) => { // index page mananger, when user clicks log
         </Card>
         <Text style={styles.titleHomepage}>YOUR MARKETPLACE</Text>
         <Divider style={styles.dividerHomepage} />
-        <Card>
-          <Card.Title>Check out the store</Card.Title>
-          <Card.Divider/>
-                <Text style={{ marginBottom: 10}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam hendrerit pharetra sodales. In viverra risus vel elit efficitur mollis. Aliquam tempor justo in lorem mattis, in pharetra magna porttitor. Nulla mattis eget augue finibus aliquam. Quisque volutpat, ipsum et dictum volutpat, leo urna volutpat urna, vitae rutrum dolor metus vel ex.</Text>
-                <Button
-                  color='#ff66ff'
-                  icon={<Icon name='code' color='#ffffff' />}
-                  buttonStyle={{borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0, color: '#ff'}}
-                  title='VIEW NOW' onPress={toggleOverlay}/>
-        </Card>
+        <Text>Todays date: {today}</Text>
+        <Text>Amount of purchases: </Text>
+        <Text>Currently owned foods: 1</Text>
+        <Text>Current cash amount: €10000</Text>
       </ScrollView>   
 
     </View>
@@ -280,7 +279,14 @@ const YourProfile = ({navigation}) => { // the users own profile, view username,
       Alert.alert('Invalid input', 'Passwords dont match!');
     }
     else{
-      Alert.alert("Success", "Password updated");
+      // get password from input
+      const user = firebase.auth().currentUser;
+      user.updatePassword(password).then(function() {
+        toggleOverlay();
+      }).catch(function(error) {
+        Alert.alert('Error', 'An error occured please try again.');
+        console.log(error);
+      });
     }
   }
 
@@ -388,10 +394,11 @@ const styles = StyleSheet.create({
     width: '20%'
   },
   dividerHomepage: {
-    backgroundColor: '#ff66ff'
+    backgroundColor: '#ff66ff',
+    height: 2
   },
   titleHomepage: {
-    padding: 5,
+    padding: 15,
     fontSize: 20, 
     fontWeight: 'bold'
   }
